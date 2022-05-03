6d3bea50eba6102a0e77709e75db87f1a4d6c64b
IgorKilipenko <igorkili@mail.ru>
2022-05-03 07:23:32 +0700

    add flutter_libserialport

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_libserialport/flutter_libserialport_plugin.h>
 #include <flutter_rtklib/flutter_rtklib_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterLibserialportPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterLibserialportPlugin"));
   FlutterRtklibPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterRtklibPlugin"));
 }
