81a2efd51af6f20c4bb6eb94fdc001fdb50b9334
IgorKilipenko <igorkili@mail.ru>
2022-05-06 04:04:29 +0700

    sfix

--- example/lib/src/app.dart
+++ example/lib/src/app.dart
@@ -45,7 +45,7 @@ class App extends StatelessWidget {
           darkTheme: ThemeData.dark().copyWith(), //ThemeData.from(colorScheme: const ColorScheme.dark()),
           themeMode: settingsController.themeMode,
 
-          initialRoute: PageRoutes.uartConnectorRoute,
+          initialRoute: !GetPlatform.isAndroid ? PageRoutes.uartConnectorRoute : PageRoutes.ubloxRoute,
           getPages: PageRoutes.getPages(settingsController),
         );
       },
