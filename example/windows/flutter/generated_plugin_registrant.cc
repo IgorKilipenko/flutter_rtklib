642d25dcf592092b50589c2a0c122ba31d55e88b
IgorKilipenko <igorkili@mail.ru>
2022-05-09 19:50:38 +0700

    add path and build_runner for next

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_libserialport/flutter_libserialport_plugin.h>
+#include <flutter_libserialport/flutter_libserialport_plugin_c_api.h>
 #include <flutter_rtklib/flutter_rtklib_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FlutterLibserialportPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterLibserialportPlugin"));
+  FlutterLibserialportPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterLibserialportPluginCApi"));
   FlutterRtklibPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterRtklibPlugin"));
 }
