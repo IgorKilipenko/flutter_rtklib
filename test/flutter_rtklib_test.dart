e7d58ae9efb5aa945fb51fee24d1e82c1b7da3e4
IgorKilipenko <igorkili@mail.ru>
2022-05-09 19:53:49 +0700

    nfix

--- test/flutter_rtklib_test.dart
+++ test/flutter_rtklib_test.dart
@@ -1,23 +1,40 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_test/flutter_test.dart' as testing;
 import 'package:flutter_rtklib/flutter_rtklib.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_rtklib');
 
-  TestWidgetsFlutterBinding.ensureInitialized();
+  testing.TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
+  testing.setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
       return '42';
     });
   });
 
-  tearDown(() {
+  testing.tearDown(() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await FlutterRtklib.platformVersion, '42');
+  testing.test('getPlatformVersion', () async {
+    testing.expect(await FlutterRtklib.platformVersion, '42');
+  });
+
+  testing.test('Test ublox', () async {
+    final environment = Platform.environment;
+    print(environment);
+    final executableArguments = Platform.executableArguments;
+    final localHostname = Platform.localHostname;
+    final packageConfig = Platform.packageConfig;
+    final resolvedExecutable = Platform.resolvedExecutable;
+    final script = Platform.script.toString();
+    final bool isTest = Platform.environment.containsKey("FLUTTER_TEST");
+    print(resolvedExecutable);
+
+    UbloxImpl ublox = UbloxImpl();
+    testing.expect(ublox, testing.isA<UbloxImpl>());
   });
 }
