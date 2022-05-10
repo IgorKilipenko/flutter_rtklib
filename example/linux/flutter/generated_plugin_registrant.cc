d03b26fade4493d9f1f8bd9275d02a54de97ddb4
IgorKilipenko <igorkili@mail.ru>
2022-05-11 00:12:22 +0700

    fix for linux

---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_libserialport/flutter_libserialport_plugin.h>
+#include <flutter_rtklib/flutter_rtklib_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_libserialport_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterLibserialportPlugin");
+  flutter_libserialport_plugin_register_with_registrar(flutter_libserialport_registrar);
+  g_autoptr(FlPluginRegistrar) flutter_rtklib_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterRtklibPlugin");
+  flutter_rtklib_plugin_register_with_registrar(flutter_rtklib_registrar);
+}
