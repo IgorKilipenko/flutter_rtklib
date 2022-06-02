87298b9e806117c88b5df611c16a6a74202412a1
IgorKilipenko <igorkili@mail.ru>
2022-06-02 20:24:15 +0700

    add matsprint test

--- test/trace_test.dart
+++ test/trace_test.dart
@@ -68,5 +68,40 @@ void main() {
       testing.expect(ouputMsg!.replaceAll(RegExp(r'[\n\r]+'), ""),
           testing.equalsIgnoringCase(format));
     });
+    testing.test("* Test matsprint", () async {
+      const columns = 3;
+      const rows = 3;
+      final matrix = <double>[
+        803.383194,
+        713.453294,
+        511.618281,
+        579.933871,
+        843.178396,
+        589.885015,
+        186.306597,
+        548.126743,
+        126.973781,
+      ];
+      pkg_ffi.using((arena) {
+        //final matrix = arena<ffi.Double>(columns*rows);
+        final matrixPtr = matrix.toNativeArray(allocator: arena);
+        CStringArray buffer = arena<ffi.Pointer<ffi.Char>>();
+        int len = rtklib.matsprint(matrixPtr, columns, rows, 5, 6, buffer);
+
+        testing.expect(len, testing.isPositive);
+        testing.expect(buffer, testing.isNot(ffi.nullptr));
+        testing.expect(buffer.value, testing.isNot(ffi.nullptr));
+        
+        final stringMatrix =
+            buffer.value.cast<pkg_ffi.Utf8>().toDartString(length: len);
+        const snapshot = ""
+            "803.383194 579.933871 186.306597\n"
+            "713.453294 843.178396 548.126743\n"
+            "511.618281 589.885015 126.973781\n"
+            "";
+
+        testing.expect(stringMatrix, testing.equalsIgnoringWhitespace(snapshot));
+      });
+    });
   });
 }
