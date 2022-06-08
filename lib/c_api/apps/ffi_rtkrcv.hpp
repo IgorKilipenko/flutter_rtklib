1d020349051620cfe5e357ce7fc79aff5254e066
IgorKilipenko <igorkili@mail.ru>
2022-06-09 03:43:11 +0700

    add ffi_rtkrcv_common.cpp file

--- lib/c_api/apps/ffi_rtkrcv.hpp
+++ lib/c_api/apps/ffi_rtkrcv.hpp
@@ -12,7 +12,25 @@ EXPORT void rtkrcv_printCmdHelpInfo(void);
 
 EXPORT int rtkrcv_readcmd(const char *file, char *cmd, int type);
 EXPORT int rtkrcv_startsvr(void);
+EXPORT int rtkrcv_rtksvrstart(rtksvr_t *svr, int cycle, int buffsize, int *strs,
+                       char **paths, int *formats, int navsel, char **cmds,
+                       char **cmds_periodic, char **rcvopts, int nmeacycle,
+                       int nmeareq, const double *nmeapos, prcopt_t *prcopt,
+                       solopt_t *solopt, stream_t *moni, char *errmsg);
 EXPORT int run_rtkrcv_cmd(int argc, char **argv);
+EXPORT void rtksvr_lock  (rtksvr_t *svr);
+EXPORT void rtksvr_unlock(rtksvr_t *svr);
+
+EXPORT void rtkrcv_start_rtksvrthread(rtksvr_t *svr);
+EXPORT int rtkrcv_decoderaw(rtksvr_t *svr, int index);
+EXPORT void rtkrcv_decodefile(rtksvr_t *svr, int index);
+EXPORT void rtkrcv_corr_phase_bias(obsd_t *obs, int n, const nav_t *nav);
+EXPORT void rtkrcv_periodic_cmd(int cycle, const char *cmd, stream_t *stream);
+EXPORT void rtkrcv_writesolhead(stream_t *stream, const solopt_t *solopt);
+EXPORT void rtkrcv_writesol(rtksvr_t *svr, int index);
+EXPORT void rtkrcv_send_nmea(rtksvr_t *svr, uint32_t *tickreset);
+EXPORT void rtkrcv_saveoutbuf(rtksvr_t *svr, uint8_t *buff, int n, int index);
+EXPORT void rtkrcv_readant(prcopt_t *opt, nav_t *nav, filopt_t *filopt );
 
 #ifdef __cplusplus
 }
