33d23d8eda9d2c071f460a717ac34aae3ad95883
IgorKilipenko <igorkili@mail.ru>
2022-06-09 15:08:12 +0700

    extends MethodChannel

--- lib/flutter_rtklib.dart
+++ lib/flutter_rtklib.dart
@@ -1,24 +1,14 @@
 // flutter_rtklib
 library flutter_rtklib;
 
-import 'dart:async';
-
-import 'package:flutter/services.dart';
+import 'package:flutter_rtklib/flutter_rtklib_method_channel.dart';
 import 'package:flutter_rtklib/src/bindings/rtklib.dart';
 export 'src/rcv/ublox/ublox.dart';
-//export 'src/bindings/rtklib.dart';
 export 'src/rtklib_bindings.dart' hide RtkDylib;
 export 'src/bindings/windows_overrides.dart';
 export 'ffi_extensions.dart';
 
-class FlutterRtklib {
-  static const MethodChannel _channel = MethodChannel('flutter_rtklib');
-
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
+class FlutterRtklib extends MethodChannelFlutterRtklib{
   static RtkLib getRtkLibInstance({int? traceLevel}) {
     return RtkLib.getInstance(traceLevel: traceLevel);
   }
--- test/flutter_rtklib_method_channel_test.dart
+++ test/flutter_rtklib_method_channel_test.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/services.dart';
+import 'package:flutter_rtklib/flutter_rtklib.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_rtklib/flutter_rtklib_method_channel.dart';
 
 void main() {
-  MethodChannelFlutterRtklib platform = MethodChannelFlutterRtklib();
-  const MethodChannel channel = MethodChannel('flutter_rtklib');
+  final platform = FlutterRtklib();
+  final channel = platform.methodChannel;
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
